90c9ecdda4774956951f78205df788482a26d76e
strawberrymochii <ellendamili@gmail.com>
2025-10-31 00:42:19 -0400

    gradient

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ var ctx = canvas.getContext("2d");
 var width = ctx.canvas.width;
 var height = ctx.canvas.height;
 var amplitude = 40;
+const gradient = ctx.createLinearGradient(0, 0, width, 0)
+
+const color_picker = document.getElementById("color")
+let currentGradient = null;
 
 var timepernote = 0;
 var length = 0;
@@ -34,37 +38,73 @@ notes.set("B", 493.9);
 oscillator.start();
 gainNode.gain.value = 0;
 
+function createColorInput(value = #ffffffff){
+    const container = document.getElementById("colors");
+    const wrapper = document.createElement("div");
+    wrapper.className = 'color-wrapper';
+
+    const input = document.createElement("input");
+    input.type = "color";
+    input.value = value;
+    input.addEventListener('input', applyGradient);
+
+    const remove = document.createElement("button");
+    remove.type = "button";
+    remove.textContent = 'X';
+    remove.addEventListener('click', () => wrapper.remove());
+
+    wrapper.appendChild('input');
+    wrapper.appendChild('remove');
+    container.appendChild('wrapper');
+
+    return input;
+
+
+}
+
+function addColorPicker(){
+    createColorInput(#ffffffff);
+    applyGradient();
+}
+
+function applyGradient(){
+
+}
+
 function frequency(pitch){
 
     freq = pitch/10000;
-    gainNode.gain.setValueAtTime(100, audioCtx.currentTime);
+    gainNode.gain.setValueAtTime(1, audioCtx.currentTime);
     oscillator.frequency.setValueAtTime(pitch, audioCtx.currentTime);
-    gainNode.gain.setValueAtTime(0, (audioCtx.currentTime + (timepernote/1000 - 0.1)));
+    gainNode.gain.setValueAtTime(0, (audioCtx.currentTime + ((timepernote/1000) - 0.1)));
 
 }
 
 function handle(){
-    length = usernotes.length;
-    timepernote = (6000/length);
-    reset = true;
     audioCtx.resume();
+    reset = true;
     gainNode.gain.value = 0;
     var usernotes = String(input.value);
-    var list = new Array;
+    length = usernotes.length;
+    timepernote = (6000/length);
+    var list = [];
 
-    for (var count = 0; count > a.length(); count++){
-        list.push(notenames.get(usernotes.charAt(count)));
+    for (i = 0; i < usernotes.length; i++) {
+        list.push(notes.get(usernotes.charAt(i)));
     }
 
-    let j = 0;
+    frequency(parseInt(list[0]));
+    drawWave();
+
+    let j = 1;
     repeat = setInterval(() => {
-        if (j > list.length){
+        if (j < list.length){
             frequency(parseInt(list[j]));
-            j++;
             drawWave();
+            j++;
         }
         else {
-            clearInterval(interval);
+            clearInterval(repeat);
         }
     }, timepernote)
 }
@@ -73,7 +113,9 @@ var counter = 0;
 
 function drawWave(){
 
-    if (reset == true){
+    clearInterval(interval);
+
+    if (reset){
         ctx.clearRect(0, 0, width, height);
         x = 0;
         y = height/2;
@@ -90,16 +132,15 @@ function drawWave(){
 
 function line(){
 
-    if (counter > (timepernote/20)){
-        clearInterval(interval);
-    }
-
     counter++;
     y = (height/2) + (amplitude*Math.sin(2*Math.PI*freq*x*(0.5*length)));
     ctx.lineTo(x, y);
+    ctx.strokeStyle = color_picker.value;
     ctx.stroke();
     x = x+1;
 
-
+    if (counter > (timepernote/20)){
+        clearInterval(interval);
+    }
 }
 
